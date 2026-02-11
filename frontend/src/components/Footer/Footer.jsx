d5d1d3c1fda86dde261c530ce30a3913a79b2d6e
Bereket Yakob <bereketyakob93@gmail.com>
2026-02-11 22:52:28 +0300

    Added footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/frontend_assets/assets";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+            inventore corporis rem quidem id itaque quod rerum maiores sint,
+            impedit iure nesciunt reiciendis, qui in voluptatum perspiciatis
+            explicabo numquam eveniet.
+          </p>
+          <div className="footer-social-links">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>CONTACT US</h2>
+          <p>Phone: +251 912345678</p>
+          <p>Email: info@maahedelivery.com</p>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        &copy; 2026 Maahedelivery. All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
