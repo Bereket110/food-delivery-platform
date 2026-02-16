c13aea7363acfe49ede23404ee4a32af73be2b5d
Bereket Yakob <bereketyakob93@gmail.com>
2026-02-16 12:35:30 +0300

    Added Admin Routing

---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
+const Sidebar = () => {
+  return (
+    <>
+      <div className="side-bar">
+        <div className="side-bar-options">
+          <NavLink to="/add" className="side-bar-option">
+            <img src={assets.add_icon} alt="" />
+            <p>Add Items</p>
+          </NavLink>
+          <NavLink to="/list" className="side-bar-option">
+            <img src={assets.order_icon} alt="" />
+            <p>List Items</p>
+          </NavLink>
+          <NavLink to="/order" className="side-bar-option">
+            <img src={assets.order_icon} alt="" />
+            <p>Order</p>
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
